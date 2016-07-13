a3f64d7d9c66b4bc6676f24db99f71e048cd9cc0
Slawomir Borowiec <przodownik@tlen.pl>
2016-07-13 20:11:43 +0200

    spring-boot-admin activated

--- src/main/java/pl/java/scalatech/SpringBootKataApplication.java
+++ src/main/java/pl/java/scalatech/SpringBootKataApplication.java
@@ -1,9 +1,14 @@
 package pl.java.scalatech;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminServer
 public class SpringBootKataApplication {
 
     public static void main(String[] args) {
