cd03e796f947319ad3756f84c90e2d6098a45931
Slawomir Borowiec <przodownik@tlen.pl>
2016-08-02 10:46:25 +0200

    sift filter

--- src/main/java/pl/java/scalatech/filters/SimpleFilter.java
+++ src/main/java/pl/java/scalatech/filters/SimpleFilter.java
@@ -1,6 +1,7 @@
 package pl.java.scalatech.filters;
 
 import java.io.IOException;
+import java.util.Random;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,10 +10,14 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.slf4j.MDC;
+
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SimpleFilter implements Filter{
 
+    private  Random random = new Random(); 
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
      log.info("init simpleFilter");
@@ -22,7 +27,20 @@ public class SimpleFilter implements Filter{
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
            log.info( "req : filter ok !");
-           chain.doFilter(request, response);
+           
+           if(random.nextBoolean()==true){
+               MDC.put("userid","przodownik" );
+               log.info("+++ przodownik");               
+           }else{
+               MDC.put("userid", "tyson");
+               log.info("+++ tyson");
+           }
+           
+           try {
+               chain.doFilter(request, response);
+           } finally {
+               MDC.remove("userid");
+           }
         
     }
 
