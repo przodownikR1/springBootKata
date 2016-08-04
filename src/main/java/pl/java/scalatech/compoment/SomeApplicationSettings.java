dc7d5514b36b1b880cc2d6d9c3ec487aa1a25921
Slawomir Borowiec <przodownik@tlen.pl>
2016-08-04 13:37:03 +0200

    ConfigurationProperties

---
+++ src/main/java/pl/java/scalatech/compoment/SomeApplicationSettings.java
@@ -0,0 +1,32 @@
+package pl.java.scalatech.compoment;
+
+import java.math.BigDecimal;
+import java.net.URL;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+@Data
+public class SomeApplicationSettings {
+
+    @NotNull
+    private URL apiUrl;        
+    @NotNull
+    private String apiKey;
+  
+    @Digits(integer = 12, fraction = 0)    
+    private int connectionTimeout;
+  
+    private String salary;
+    
+    private int count;
+    
+    private boolean useSSL;
+}
---
+++ src/test/java/pl/java/scalatech/properties/SimplePropTest.java
@@ -0,0 +1,48 @@
+package pl.java.scalatech.properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.slf4j.Slf4j;
+import pl.java.scalatech.compoment.SomeApplicationSettings;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=SimplePropTest.SimpleConfing.class)
+@Slf4j
+public class SimplePropTest {
+    
+    @Autowired
+    private SomeApplicationSettings settings;
+    
+    @Value("${user.name}")
+    private String name;
+    
+    @Test
+    public void shouldBootstrap(){
+        
+    }
+    @Test
+    public void shouldInjectByValue(){
+        assertThat(name).isEqualTo("przodownik");
+    }
+    @Test
+    public void shouldConfigurationPropertiesWork(){
+        log.info("{}",settings);
+    }
+    
+    
+    @Configuration   
+    @ComponentScan(basePackageClasses={SomeApplicationSettings.class})
+    static class SimpleConfing{
+        
+    }
+    
+}
