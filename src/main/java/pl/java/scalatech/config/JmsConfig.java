52ecc611b2a760a54efb09396a1114e242898506
Sławek Borowiec <przodownik@tlen.pl>
2016-08-24 14:22:38 +0200

    remote broker work

--- src/main/java/pl/java/scalatech/config/JmsConfig.java
+++ src/main/java/pl/java/scalatech/config/JmsConfig.java
@@ -5,16 +5,22 @@ import javax.jms.Queue;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableScheduling
+@EnableJms
+@Slf4j
 public class JmsConfig {
 
     public static final String SAMPLE_QUEUE = "sample1.queue";
 
     @Bean
     public Queue queue() {
+        log.info("+++ create queue,.....");
         return new ActiveMQQueue(SAMPLE_QUEUE);
     }
 }
